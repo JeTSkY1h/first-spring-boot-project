f3f3d37284981385a9f063f7894e4cda90baaff9
JeTSkY1h <jetsky1h@googlemail.com>
2022-06-01 16:47:55 +0200

    github exercise

--- src/main/java/com/example/firstspringbootproject/GitHubController.java
+++ src/main/java/com/example/firstspringbootproject/GitHubController.java
@@ -1,10 +1,21 @@
 package com.example.firstspringbootproject;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/github")
 public class GitHubController {
+    
+    @GetMapping("/{user}")
+    public List<String> getRepos(@PathVariable String user){
+        RestTemplate restTemplate = new RestTemplate();
+        return Arrays.stream(restTemplate.getForObject("https://api.github.com/users/" + user + "/repos", Repos[].class))
+                .map(r -> r.getName()).toList();
+
+    }
+
+
 
 }
